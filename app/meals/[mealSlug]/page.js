76aeeb886635f0b13b3c71f318193c6aa3f56e4f
Steve Tyler <steve.tyler75@gmail.com>
2026-01-07 22:31:10 +0000

    section 443

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,9 @@
+export default function MealsDetailsPage() {
+  return (
+    <main>
+      <h1>
+        Meals Details Page
+      </h1>
+    </main>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function ShareMealPage() {
+  return (
+    <main>
+      <h1>
+        Share Meal Page
+      </h1>
+    </main>
+  );
+}
