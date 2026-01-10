5d25dbe5055e420f3514b3e5a9cf89f25e31f680
Steve Tyler <steve.tyler75@gmail.com>
2026-01-10 15:22:20 +0000

    section 445

---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <MainHeaderBackground />
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="Foodies App Logo" priority/>
+        </Link>
+        <h1>Next Level Food</h1>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
