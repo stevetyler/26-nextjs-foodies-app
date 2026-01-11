42e3a9b4cee9aeaa6ace4913139147ac13c16801
Steve Tyler <steve.tyler75@gmail.com>
2026-01-11 20:45:33 +0000

    section 452

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,26 +1,29 @@
 import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
+import { getMeals } from '../lib/meals';  
 
-export default function MealsPage() {
+export default async function MealsPage() { // can use async here for server components
+  const meals = await getMeals();
+
   return (
-   <>
-    <header className={classes.header}>
-      <h1>
-        Delicious Meals created <span className={classes.highlight}>by you</span>
-      </h1>
-      <p>
-        Share your favorite recipes with the community and explore meals made by others.
-      </p>
-      <p className={classes.cta}>
-        <Link href="/meals/share">
-          Share your favourite recipe
-        </Link>
-      </p>
-    </header>
-    <main className={classes.main}>
-      <MealsGrid meals={[]} />
-    </main>
-   </>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Share your favorite recipes with the community and explore meals made by others.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share your favourite recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={ meals } />
+      </main>
+    </>
   );
 }
