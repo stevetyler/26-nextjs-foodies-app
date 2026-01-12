f7f0fa6569ef60c43babf6c1b2f32247a3582e53
Steve Tyler <steve.tyler75@gmail.com>
2026-01-12 21:34:45 +0000

    section 456

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound () {
+    return <main className="not-found">
+        <h1>Meals Not Found</h1>
+        <p>We couldn't find any meals. Please try again later.</p>
+    </main>;
+}
