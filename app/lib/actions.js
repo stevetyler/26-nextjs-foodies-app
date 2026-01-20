34f2f2828888620ad508ff67c1c28fde06c29e22
Steve Tyler <steve.tyler75@gmail.com>
2026-01-20 20:27:27 +0000

    section 469

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -4,7 +4,11 @@ import { redirect } from 'next/navigation';
 
 import { saveMeal } from './meals';
 
-export async function shareMeal(formData) {
+function isInvalidtext(text) {
+  return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData ) { // useFormState requires 2 parameters
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -13,6 +17,20 @@ export async function shareMeal(formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email'),
   };
+
+  if(
+    isInvalidtext(meal.title) ||
+    isInvalidtext(meal.summary) ||
+    isInvalidtext(meal.instructions) ||
+    isInvalidtext(meal.creator) ||
+    isInvalidtext(meal.creator_email) ||
+    !meal.image || meal.image.size === 0
+  ) {
+    //throw new Error('Title, summary, instructions, name, email, and image are required.'); // will discard form submission values
+    return {
+      message: 'Title, summary, instructions, name, email, and image are required.'
+    }
+  }
 
   await saveMeal(meal);
   redirect('/meals');
---
+++ app/meals/share/error.js
@@ -0,0 +1,8 @@
+"use client";
+
+export default function Error() {
+  return <main className="error">
+    <h1>Something went wrong!</h1>
+    <p>Failed to create meal. Please check and try again</p>
+  </main>;
+}
